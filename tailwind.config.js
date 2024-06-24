a6e03cce58d9bf79991a9983c56bcb969d79785c
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-06-25 00:25:57 +0700

    feat: can add, delete and pop  before user delete

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: " #46a5fa",
+        secondary: "#efefef",
+      },
+    },
+  },
+  plugins: [],
+});
